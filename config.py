23674542e1a4d1726ad429fc56c8b13738b5cf4e
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-06-03 09:35:49 +0300

    обновленный путь к серверу

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-VIDEO_DIRECTORY = Path(r"D:\veFox")
+VIDEO_DIRECTORY = Path(r"E:\server\veFox")
 
 ALLOWED_VIDEO_SUFFIX = {".mp4", ".mkv", ".avi", ".mov"}
 ALLOWED_SUBTITLES_SUFFIX = {".vtt"}
